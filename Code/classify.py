e51257e0718e5d82eb0c1f20db6b987286f56526
caracheng <ccheng@macalester.edu>
2017-03-18 18:19:25 -0500

    Struggle bus

--- Code/classify.py
+++ Code/classify.py
@@ -5,22 +5,32 @@ import preClassify
 #Based off of http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
 
 
-def collectComments(path):
+def collectComments(path, sentiment):
     allComments = []
     with open(path, 'rb') as commentFile:
         commentsFem = csv.reader(commentFile)
         for row in commentsFem:
             for comment in row:
-                allComments.append(comment)
-    return allComments
+                allComments.append(comment + "," + sentiment)
+    print allComments
 
-pos_comments = collectComments('pos_comments.csv')
+pos_comments = collectComments('pos_comments.csv', "positive")
 
-neg_comments = collectComments('neg_comments.csv')
+pos_comments
+
+sug_comments = collectComments('sug_comments.csv', "suggestive")
+
+unrelated_comments = collectComments('unrelated_comments.csv', "unrelated")
 
 def makeComments():
     comments = []
-    for (words, sentiment) in pos_comments + neg_comments:
+    for (words, sentiment) in pos_comments:
+        words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+        comments.append((words_filtered, sentiment))
+    for (words, sentiment) in sug_comments:
+        words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+        comments.append((words_filtered, sentiment))
+    for (words, sentiment) in unrelated_comments:
         words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
         comments.append((words_filtered, sentiment))
     return comments
